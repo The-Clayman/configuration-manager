f5ec35ab03e189b63336efd536f70270db47c4ba
Roy <zurroy@gmail.com>
2022-01-31 22:52:44 +0200

    Add env resolve, get files list, get file content Rest api
    add resolve_param_from_env with fallback to resolve data folder for start
    add get_files Rest api to return a list of available files
    add get_file Rest api to return file content by file name

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,9 @@ MAINTAINER The-Clayman https://github.com/The-Clayman
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
+# create data folder for configuration files
+RUN mkdir /Data
+
 COPY . .
 
 CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
@@ -13,5 +16,5 @@ CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
 # docker build -t configuration-manager:v1 .
 
 # run command
-# docker run -d --name configuraion-manager -p 5000:5000 configuration-manager:v1
+# docker run -d --name configuraion-manager -p 5000:5000 -v <data_path_on_host>:/Data configuration-manager:v1
 
--- app.py
+++ app.py
@@ -1,18 +1,51 @@
 __version__ = "1.0.0"
 
-from flask import Flask, Response
+import os
+from os import listdir
+from flask import Flask, Response, send_from_directory
+
+# env param after resolve
+env_params = {}
 
 app = Flask(__name__)
 
-# wrap message in json body
+
+# Resolve param from env, with fallback to default values
+@app.before_first_request
+def resolve_param_from_env():
+    global env_params
+
+    # resolve env params, with default fallbacks
+    env_params["data_folder_location"] = os.getenv("DATA_FOLDER", "/Data")
+
+    app.logger.warning("Env param loaded, with fall back to defaults")
+    app.logger.warning(env_params)
+
+
+# Wraps message in json response body, with status
 def wrap_message_response_in_json(message, status):
     response = Response(response=f"{{ \"message\" :\"{message}\" }}", status=status)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+
 @app.route("/configuration_manager/v1/version", methods=['GET'])
 def get_version():
     return wrap_message_response_in_json(f"Configuration Manager, version:[{__version__}]", 200)
+
+
+# Returns a list of files exist under data folder
+@app.route("/configuration_manager/v1/files", methods=['GET'])
+def get_files():
+    path = env_params["data_folder_location"]
+    return wrap_message_response_in_json(f"Available files: {listdir(path)}", 200)
+
+
+# Returns file content by file_name
+@app.route("/configuration_manager/v1/file/<string:file_name>", methods=['GET'])
+def get_file(file_name):
+    path = env_params["data_folder_location"]
+    return send_from_directory(path, file_name)
 
 if __name__=="__main__":
     app.run()
